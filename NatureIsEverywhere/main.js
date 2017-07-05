3c4b47d6074b0ad414289ae4352c7c037a952fd5
Sam Ticknor <ticknors@carnegiemuseums.org>
2017-07-05 12:20:02 -0400

    Maybe fullscreen on linux and added basic timeout behavior for sparkle

--- NatureIsEverywhere/main.js
+++ NatureIsEverywhere/main.js
@@ -13,8 +13,9 @@ var mapWindow = null;
 
 // CREATE WINDOWS
 app.on('ready', () => {
-    mapWindow = new BrowserWindow({ width: 1920, height: 1080, resizable: false });
+    mapWindow = new BrowserWindow({ width: 1920, height: 1080, resizable: false, type:'toolbar', fullscreen: true, frame: false, 'auto-hide-menu-bar': true, });
     mapWindow.loadURL(`file://${__dirname}/map.html`);
+    mapWindow.setMenu(null);
 });
 
 //////////////////////// EVENTS ////////////////////////
