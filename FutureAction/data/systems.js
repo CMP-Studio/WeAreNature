88e5d7cb8ae4952b5f0da7da958c72a26c141743
Ticknor, Samantha <TicknorS@CarnegieMuseums.Org>
2017-09-15 15:21:11 -0400

    Edits to an almost done interaction

--- FutureAction/data/systems.js
+++ FutureAction/data/systems.js
@@ -6,17 +6,17 @@ var systems = {
         {
           name: 'Ride my bike.',
           text: 'You’ll have more fun getting there, and save carbon dioxide emissions along the way.',
-          localOrg: 'Bike Pittsburgh is a local organization working to transform streets and communities into vibrant, healthy places by making them safe and accessible for people to bike and walk. Joining a group helps your individual actions influence systems at a larger scale.',
+          localOrg: '<span class="bodyBold">Bike Pittsburgh</span> is a local organization working to transform streets and communities into vibrant, healthy places by making them safe and accessible for people to bike and walk. Joining a group helps your individual actions influence systems at a larger scale.',
         },
         {
           name: 'Plan for parking.',
           text: 'Save time and cut emissions produced from idling and circling to find parking.',
-          localOrg: 'The Pittsburgh Parking Authority is experimenting with sensors that can tell you where parking is full and where you can find a space. When transit systems can get information to the public, you can make informed decisions that save you time and improve air quality.',
+          localOrg: '<span class="bodyBold">The Pittsburgh Parking Authority</span> is experimenting with sensors that can tell you where parking is full and where you can find a space. When transit systems can get information to the public, you can make informed decisions that save you time and improve air quality.',
         },
         {
           name: 'Report air quality issues.',
-          text: 'Your observations can help scientists improve air quality.',
-          localOrg: 'Scientists at the Allegheny County Department of Health need your help to collect data on air quality. With the Smell Pittsburgh mobile phone app designed by the CREATE Lab at Carnegie Mellon University, you contribute to a citizen science project that will help improve air quality.',
+          text: 'Your observations can help scientists improve<br>air quality.',
+          localOrg: 'Scientists at the <span class="bodyBold">Allegheny County Department of Health</span> need your help to collect data on air quality. With the <span class="bodyBold">Smell Pittsburgh</span> mobile phone app designed by the CREATE Lab at Carnegie Mellon University, you contribute to a citizen science project that will help improve air quality.',
         },
       ]
     },
@@ -26,18 +26,18 @@ var systems = {
       actions: [
         {
           name: 'Do an energy audit.',
-          text: 'Small changes in your home (like closing shades on sunny days) can save money on energy bills.',
-          localOrg: 'Conservation Consultants Inc. is a nonprofit that helps homeowners and small businesses address environmental concerns. The Home Energy Saver project can be used for a DIY evaluation. Every kilowatt we save counts in a region that relies on fossil fuels for much of our energy.',
+          text: 'Small changes in your home (like closing shades<br>on sunny days) can save money on energy bills.',
+          localOrg: '<span class="bodyBold">Conservation Consultants Inc.</span> is a nonprofit that helps homeowners and small businesses address environmental concerns. The Home Energy Saver project can be used for a DIY evaluation. Every kilowatt we save counts in a region that relies on fossil fuels for much of our energy.',
         },
         {
           name: 'Support renewable energy.',
-          text: 'Learn more about alternative energy options that are available to you.',
-          localOrg: 'Solar power generates energy without carbon emissions, and creates local jobs. Anyone who pays an electric bill can opt in to plans that source electricity from renewable sources. Ask your local electricity supplier or learn more from PA PowerSwitch.',
+          text: 'Learn more about alternative energy options that<br>are available to you.',
+          localOrg: 'Solar power generates energy without carbon emissions, and creates local jobs. Anyone who pays an electric bill can opt in to plans that source electricity from renewable sources. Ask your <span class="bodyBold">local electricity supplier</span> or learn more from <span class="bodyBold">PA PowerSwitch.</span>',
         },
         {
           name: 'Try reuse.',
           text: 'Instead of using energy to make something new, upcycle materials for your next project. ',
-          localOrg: 'Find a donation center to take your old materials and provide a starter for something new. Construction Junction has materials for home renovation, and Pittsburgh Center for Creative Reuse deals with craft supplies for small creative projects.',
+          localOrg: 'Find a donation center to take your old materials and provide a starter for something new. <span class="bodyBold">Construction Junction</span> has materials for home renovation, and <span class="bodyBold">Pittsburgh Center for Creative Reuse</span> deals with craft supplies for small creative projects.',
         },
       ]
     },
@@ -47,18 +47,18 @@ var systems = {
       actions: [
         {
           name: 'Eat less beef.',
-          text: 'Eating plant-based meals instead of a burger can reduce your carbon footprint.',
-          localOrg: 'When you think about the energy, water, and fertilizer that go into meat production, the environmental impact really adds up. Grow your own food or visit a local farmer’s markets to find affordable seasonal produce and show support for sustainable agriculture.',
+          text: 'Eating plant-based meals instead of a burger<br>can reduce your carbon footprint.',
+          localOrg: 'When you think about the energy, water, and fertilizer that go into meat production, the environmental impact really adds up. Grow your own food or visit a <span class="bodyBold">local farmer’s markets</span> to find affordable seasonal produce and show support for sustainable agriculture.',
         },
         {
           name: 'Reduce food waste.',
-          text: 'Use the Food Rescue Hero app to get your leftover food to someone who needs it.',
-          localOrg: 'Did you order too much for an office meeting or special event? Fight hunger by sharing leftovers with those in need. 412 Food rescue has a variety of programs to redirect fresh food from landfills to organizations that address hunger.',
+          text: 'Use the Food Rescue Hero app to get your<br>leftover food to someone who needs it.',
+          localOrg: 'Did you order too much for an office meeting or special event? Fight hunger by sharing leftovers with those in need. <span class="bodyBold">412 Food Rescue</span> has a variety of programs to redirect fresh food from landfills to organizations that address hunger.',
         },
         {
           name: 'Improve my soil.',
-          text: 'Try composting to reduce greenhouse gasses and help plants grow.',
-          localOrg: 'When organic materials decompose in landfill environments that lack oxygen, it creates methane gas that contributes to global warming. The Pennsylvania Resources Council offers low-cost classes on composting that include all required start-up materials. You can compost in your apartment or outdoors: less landfill waste benefits everyone.',
+          text: 'Try composting to reduce greenhouse gasses<br>and help plants grow.',
+          localOrg: 'When organic materials decompose in landfill environments that lack oxygen, it creates methane gas that contributes to global warming. <span class="bodyBold">The Pennsylvania Resources Council</span> offers low-cost classes on composting that include all required start-up materials. You can compost in your apartment or outdoors: less landfill waste benefits everyone.',
         },
       ]
     },
@@ -69,16 +69,16 @@ var systems = {
         {
           name: 'Plant a tree.',
           text: 'Trees provide shade, look great, and help prevent sewer system overflows.',
-          localOrg: 'One tree can soak up 100 gallons of water. Collectively, the trees across an urban landscape can keep a significant amount of water out of the sewer system. Tree Pittsburgh is dedicated to enhancing community vitality by restoring and protecting the urban forest.',
+          localOrg: 'One tree can soak up 100 gallons of water. Collectively, the trees across an urban landscape can keep a significant amount of water out of the sewer system. <span class="bodyBold">Tree Pittsburgh</span> is dedicated to enhancing community vitality by restoring and protecting the urban forest.',
         },
         {
           name: 'Try Kayaking.',
-          text: 'River recreation motivates people, businesses, and government to keep our rivers clean.',
+          text: 'River recreation motivates people, businesses,<br>and government to keep our rivers clean.',
           localOrg: 'Riverfront access has never been better. By taking part in the many activities available along Pittsburgh’s waterfront, you show those in local government what is important to its people, and where money should be invested for further improvements.',
         },
         {
           name: 'Replace plastics with reusables.',
-          text: 'Use a refillable water bottle or take your own bags to the grocery store.',
+          text: 'Use a refillable water bottle or take your own<br>bags to the grocery store.',
           localOrg: 'Our local rivers are part of a much larger watershed. Disposable plastics like bottles and straws end up in our waterways and will eventually flow to the Mississippi River and into the Gulf of Mexico. We may think of ourselves as landlocked, but our trash can end up in the ocean.',
         },
       ]
@@ -90,7 +90,7 @@ var systems = {
         {
           name: 'Help birds.',
           text: 'Learn about techniques to minimize bird-building collisions.',
-          localOrg: 'Birds can thrive in cities, but sometimes have fatal collisions with reflective glass on buildings. BirdSafe Pittsburgh coordinates local research efforts to reduce bird-glass collisions and increase healthy habitats for birds.',
+          localOrg: 'Birds can thrive in cities, but sometimes have fatal collisions with reflective glass on buildings. <span class="bodyBold">BirdSafe Pittsburgh</span> coordinates local research efforts to reduce bird-glass collisions and increase healthy habitats for birds.',
         },
         {
           name: 'Grow native plants.',
--- FutureActionPoll/javascripts/entry.js
+++ FutureActionPoll/javascripts/entry.js
@@ -1,24 +1,55 @@
 require('../less/main.less');
 
+import './index.css';
+
 'use strict';
 
 var React = require('react');
 var ReactDOM = require('react-dom');
-import { PieChart, Pie, Sector, Cell} from 'recharts';
+import { PieChart, Pie, Sector, Cell, Label} from 'recharts';
 
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const COLORS = ['#5cb7b3', '#146170', '#a65a95', '#cb7d31', '#cedb29'];
 
-const data02 = [{name: 'Group A', value: 2400}, {name: 'Group B', value: 4567},
-                  {name: 'Group C', value: 1398}, {name: 'Group D', value: 9800},
-                  {name: 'Group E', value: 3908}, {name: 'Group F', value: 4800}];
+const data02 = [{name: 'Group A', value: 2400, label: 'Water'},
+                {name: 'Group B', value: 4567, label: 'Habitat'},
+                {name: 'Group C', value: 1398, label: 'Transportation'},
+                {name: 'Group D', value: 9800, label: 'Food'},
+                {name: 'Group E', value: 3908, label: 'Energy'}];
+
+const styles = {
+  
+};
+
+
 
 var Entry = React.createClass({
+  renderLabel: function(props) {
+    return (
+      <text 
+        x={props.x > props.cx ? props.x+15 : props.x - 15}
+        y={props.y > props.cy ? props.y+15 : props.y - 15}
+        textAnchor={props.x > props.cx ? 'start' : 'end'}
+        fill={'#fefaeb'}
+        style={{
+          color: '#fefaeb',
+          fontSize: 35,
+          fontFamily: 'BrandonBody',
+          flexDirection: 'column',
+          textAlign: 'center',
+          lineHeight: 1.5,
+        }}
+      >
+          {props.label}
+      </text>);
+  },
+
   render: function(){
     return (
       <div className="myDiv">
+      
       <img 
-        src={require('../assets/background.jpg')} 
+        src={require('./assets/background.jpg')} 
         style={{
           position: 'absolute',
           top: 0,
@@ -27,20 +58,78 @@ var Entry = React.createClass({
           height: 1080,
         }}
       />
+      <div 
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: 1920,
+          height: 1080,
+          background: '#000',
+          opacity: 0.35,
+        }}
+      />
 
       <div className="Dim"> </div> 
 
 
       
-       <text> We Pledge to Protect</text>
-      <text className="pledgeCount" textAnchor="middle" fill="#82ca9d" > 203 Pledges So Far </text>
-      <PieChart className="chart" width={600} height={600}>
-        <Pie data={data02}  cx={300} cy={300} innerRadius={200} outerRadius={250} fill="#82ca9d" label>
+      <text
+        style={{
+          position: 'absolute',
+          top: 60,
+          left: 60,
+          color: '#fefaeb',
+          fontSize: 70,
+          fontFamily: 'BrandonTitle',
+        }}
+      > 
+        We pledge to protect...
+      </text>
+
+      <text 
+        className="pledgeCount"
+        textAnchor="middle"
+        fill="#82ca9d"
+        style={{
+          color: '#fefaeb',
+          fontSize: 90,
+          fontFamily: 'BrandonBody',
+          flexDirection: 'column',
+          textAlign: 'center',
+          lineHeight: 1,
+          position: 'absolute',
+          top: 1080/2 - 15,
+        }}> 
+        203{'\n'}
+        <text style={{ fontSize: 60, display: 'block', lineHeight: 1 }}>Pledges</text>
+      </text>
+
+      <PieChart
+        className="chart"
+        width={1920}
+        height={1080}
+        style={{
+          position: 'relative',
+          top: 60,
+        }}
+      >
+        <Pie
+          data={data02} 
+          cx={'50%'}
+          cy={'50%'}
+          innerRadius={225}
+          outerRadius={375}
+          fill="#82ca9d"
+          labelLine={false}
+          label={this.renderLabel}
+        >
           {
             data02.map((entry, index) => 
               <Cell 
                 key={index}
                 fill={COLORS[index % COLORS.length]}
+                stroke={'transparent'}
               />
             )
           }
