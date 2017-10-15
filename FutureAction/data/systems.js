371453f3b14669424be5c627d999b97f94c83552
Sam Ticknor <ticknors@carnegiemuseums.org>
2017-10-15 00:55:48 -0400

    Updating Future Action UI

--- FutureAction/data/systems.js
+++ FutureAction/data/systems.js
@@ -1,10 +1,10 @@
 var systems = {
-    'transportation' : {
+    'Transportation' : {
       name: 'Transportation',
       icon: 'transportation_icon.png',
       actions: [
         { 
-          id: 'bike',
+          id: 'Bike',
           name: 'Ride my bike.',
           text: 'You’ll have more fun getting there, and save carbon dioxide emissions along the way.',
           localOrgText: '<span class="bodyBold">Bike Pittsburgh</span> is a local organization working to transform streets and communities into vibrant, healthy places by making them safe and accessible for people to bike and walk. Joining a group helps your individual actions influence systems at a larger scale.',
@@ -13,7 +13,7 @@ var systems = {
           learnMoreLink: 'http://www.pittsburghpa.gov/dcp/completestreets',
         },
         {
-          id: 'parking',
+          id: 'Parking',
           name: 'Plan for parking.',
           text: 'Save time and cut emissions produced from idling and circling to find parking.',
           localOrgText: '<span class="bodyBold">The Pittsburgh Parking Authority</span> is experimenting with sensors that can tell you where parking is full and where you can find a space. When transit systems can get information to the public, you can make informed decisions that save you time and improve air quality.',
@@ -22,7 +22,7 @@ var systems = {
           learnMoreLink: 'http://gasp-pgh.org/',
         },
         {
-          id: 'airquality',
+          id: 'Airquality',
           name: 'Report air quality issues.',
           text: 'Your observations can help scientists improve<br>air quality.',
           localOrgText: 'Scientists at the <span class="bodyBold">Allegheny County Department of Health</span> need your help to collect data on air quality. With the <span class="bodyBold">Smell Pittsburgh</span> mobile phone app designed by the CREATE Lab at Carnegie Mellon University, you contribute to a citizen science project that will help improve air quality.',
@@ -33,12 +33,12 @@ var systems = {
         },
       ]
     },
-    'energy' : {
+    'Energy' : {
       name: 'Energy',
       icon: 'energy_icon.png',
       actions: [
         {
-          id: 'energyaudit',
+          id: 'Energyaudit',
           name: 'Do an energy audit.',
           text: 'Small changes in your home (like closing shades<br>on sunny days) can save money on energy bills.',
           localOrgText: '<span class="bodyBold">Conservation Consultants Inc.</span> is a nonprofit that helps homeowners and small businesses address environmental concerns. The Home Energy Saver project can be used for a DIY evaluation. Every kilowatt we save counts in a region that relies on fossil fuels for much of our energy.',
@@ -47,7 +47,7 @@ var systems = {
           learnMoreLink: 'https://www.go-gba.org/resources/green-building/',
         },
         {
-          id: 'renewable',
+          id: 'Renewable',
           name: 'Support renewable energy.',
           text: 'Learn more about alternative energy options that<br>are available to you.',
           localOrgText: 'Solar power generates energy without carbon emissions, and creates local jobs. Anyone who pays an electric bill can opt in to plans that source electricity from renewable sources. Ask your <span class="bodyBold">local electricity supplier</span> or learn more from <span class="bodyBold">PA PowerSwitch.</span>',
@@ -56,7 +56,7 @@ var systems = {
           learnMoreLink: 'https://www.pennfuture.org/clean-energy',
         },
         {
-          id: 'reuse',
+          id: 'Reuse',
           name: 'Try reuse.',
           text: 'Instead of using energy to make something new, upcycle materials for your next project. ',
           localOrgText: 'Find a donation center to take your old materials and provide a starter for something new. <span class="bodyBold">Construction Junction</span> has materials for home renovation, and <span class="bodyBold">Pittsburgh Center for Creative Reuse</span> deals with craft supplies for small creative projects.',
@@ -66,12 +66,12 @@ var systems = {
         },
       ]
     },
-    'food' : {
+    'Food' : {
       name: 'Food',
       icon: 'food_icon.png',
       actions: [
         {
-          id: 'beef',
+          id: 'Beef',
           name: 'Eat less beef.',
           text: 'Eating plant-based meals instead of a burger<br>can reduce your carbon footprint.',
           localOrgText: 'When you think about the energy, water, and fertilizer that go into meat production, the environmental impact really adds up. Grow your own food or visit a <span class="bodyBold">local farmer’s markets</span> to find affordable seasonal produce and show support for sustainable agriculture.',
@@ -80,7 +80,7 @@ var systems = {
           learnMoreLink: 'http://www.drawdown.org/solutions/food/plant-rich-diet',
         },
         {
-          id: 'foodwaste',
+          id: 'Foodwaste',
           name: 'Reduce food waste.',
           text: 'Use the Food Rescue Hero app to get your<br>leftover food to someone who needs it.',
           localOrgText: 'Did you order too much for an office meeting or special event? Fight hunger by sharing leftovers with those in need. <span class="bodyBold">412 Food Rescue</span> has a variety of programs to redirect fresh food from landfills to organizations that address hunger.',
@@ -89,7 +89,7 @@ var systems = {
           learnMoreLink: 'http://www.drawdown.org/solutions/food/reduced-food-waste',
         },
         {
-          id: 'soil',
+          id: 'Soil',
           name: 'Improve my soil.',
           text: 'Try composting to reduce greenhouse gasses<br>and help plants grow.',
           localOrgText: 'When organic materials decompose in landfill environments that lack oxygen, it creates methane gas that contributes to global warming. <span class="bodyBold">The Pennsylvania Resources Council</span> offers low-cost classes on composting that include all required start-up materials. You can compost in your apartment or outdoors: less landfill waste benefits everyone.',
@@ -97,19 +97,19 @@ var systems = {
         },
       ]
     },
-    'water' : {
+    'Water' : {
       name: 'Water',
       icon: 'water_icon.png',
       actions: [
         {
-          id: 'tree',
+          id: 'Tree',
           name: 'Plant a tree.',
           text: 'Trees provide shade, look great, and help prevent sewer system overflows.',
           localOrgText: 'One tree can soak up 100 gallons of water. Collectively, the trees across an urban landscape can keep a significant amount of water out of the sewer system. <span class="bodyBold">Tree Pittsburgh</span> is dedicated to enhancing community vitality by restoring and protecting the urban forest.',
           localOrgLink: 'https://www.treepittsburgh.org/',
         },
         {
-          id: 'kayaking',
+          id: 'Kayaking',
           name: 'Try Kayaking.',
           text: 'River recreation motivates people, businesses,<br>and government to keep our rivers clean.',
           localOrgText: 'Riverfront access has never been better. By taking part in the many activities available along Pittsburgh’s waterfront, you show those in local government what is important to its people, and where money should be invested for further improvements.',
@@ -118,7 +118,7 @@ var systems = {
           learnMoreLink: 'https://friendsoftheriverfront.org/trails/three-rivers-heritage-trail/',
         },
         {
-          id: 'plastics',
+          id: 'Plastics',
           name: 'Replace plastics with reusables.',
           text: 'Use a refillable water bottle or take your own<br>bags to the grocery store.',
           localOrgText: 'Our local rivers are part of a much larger watershed. Disposable plastics like bottles and straws end up in our waterways and will eventually flow to the Mississippi River and into the Gulf of Mexico. We may think of ourselves as landlocked, but our trash can end up in the ocean.',
@@ -127,19 +127,19 @@ var systems = {
         },
       ]
     },
-    'habitat' : {
+    'Habitat' : {
       name: 'Habitat',
       icon: 'habitat_icon.png',
       actions: [
         {
-          id: 'birds',
+          id: 'Birds',
           name: 'Help birds.',
           text: 'Learn about techniques to minimize bird-building collisions.',
           localOrgText: 'Birds can thrive in cities, but sometimes have fatal collisions with reflective glass on buildings. <span class="bodyBold">BirdSafe Pittsburgh</span> coordinates local research efforts to reduce bird-glass collisions and increase healthy habitats for birds.',
           localOrgLink: 'http://birdsafepgh.org/',
         },
         {
-          id: 'nativeplants',
+          id: 'Nativeplants',
           name: 'Grow native plants.',
           text: 'Gardening with plants that are suited to your soil saves money by decreasing the need for watering.',
           localOrgText: 'Native plants create food and shelter for local wildlife including birds, butterflies, and beneficial insects. Your garden is part of a larger habitat, including parks and land trusts, creating green corridors that support migrating wildlife.',
@@ -148,7 +148,7 @@ var systems = {
           learnMoreLink: 'http://www.alleghenyplaces.com/maps/ec/Greenways.pdf',
         },
         {
-          id: 'hike',
+          id: 'Hike',
           name: 'Take a hike.',
           text: 'Show support for local green spaces by exploring recreation in local parks.',
           localOrgText: 'There are hundreds of miles of hiking and biking trails to explore in city, county, and state parks. Your support for green public lands has additional benefits including more habitat for wildlife, protection for watersheds, and relief from urban heat islands.',
--- FutureAction/main.js
+++ FutureAction/main.js
@@ -13,30 +13,7 @@ var FromMeToWe = null;
 
 // CREATE WINDOWS
 app.on('ready', () => {
-    FromMeToWe = new BrowserWindow({ width: 1920, height: 1080, resizable: false }); // type:'toolbar', fullscreen: true, frame: false, 'auto-hide-menu-bar': true, });
+    FromMeToWe = new BrowserWindow({ width: 1920, height: 1080, resizable: false, frame: false, });
     FromMeToWe.loadURL(`file://${__dirname}/FromMeToWe.html`);
     FromMeToWe.setMenu(null);
-});
-
-//////////////////////// EVENTS ////////////////////////
-
-ipcMain.on('pledge', (event, object) => {
-    try {
-      // send pledge to database....
-      FromMeToWe.send('pledgeRecorded');
-    } catch (e) {
-      console.log(e);
-      FromMeToWe.send('pledgeNotRecorded');
-    }
-});
-
-
-ipcMain.on('reminder', (event, object) => {
-    try {
-      // send reminder info to database....
-      FromMeToWe.send('reminderRecorded');
-    } catch (e) {
-      console.log(e);
-      FromMeToWe.send('reminderNotRecorded');
-    }
 });
