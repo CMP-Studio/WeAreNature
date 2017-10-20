80d64fccea525ea57819a49be5978a6b0258aa24
Sam Ticknor <ticknors@carnegiemuseums.org>
2017-10-20 10:33:17 -0400

    Made nature is everywhere fullscreen

--- NatureIsEverywhere/main.js
+++ NatureIsEverywhere/main.js
@@ -13,7 +13,7 @@ var mapWindow = null;
 
 // CREATE WINDOWS
 app.on('ready', () => {
-  mapWindow = new BrowserWindow({ width: 1920, height: 1080, frame: false, resizeable: false });
+  mapWindow = new BrowserWindow({ width: 1920, height: 1080, resizable: false, frame: false, fullscreen: true });
   mapWindow.loadURL(`file://${__dirname}/map.html`);
   mapWindow.setResizable(false);
   mapWindow.setMenu(null);
