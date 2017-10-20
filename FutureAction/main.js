0bbc9b402c126fa56decab6f5cdafebf88238466
Sam Ticknor <ticknors@carnegiemuseums.org>
2017-10-20 09:31:10 -0400

    Made apps full screen

--- FutureAction/main.js
+++ FutureAction/main.js
@@ -3,7 +3,7 @@
 //////////////////////// IMPORTS ////////////////////////
 
 var electron = require('electron');
-var {app, BrowserWindow, ipcMain} = electron;
+var {app, BrowserWindow} = electron;
 
 //////////////////////// EXPORTS ////////////////////////
 
@@ -13,7 +13,7 @@ var FromMeToWe = null;
 
 // CREATE WINDOWS
 app.on('ready', () => {
-    FromMeToWe = new BrowserWindow({ width: 1920, height: 1080, resizable: false, frame: false, });
+    FromMeToWe = new BrowserWindow({ width: 1920, height: 1080, resizable: false, frame: false, fullscreen: true });
     FromMeToWe.loadURL(`file://${__dirname}/FromMeToWe.html`);
     FromMeToWe.setMenu(null);
 });
