b32ddf15b7129c92d80c5df7a320c2e4024d9e10
Sam Ticknor <ticknors@carnegiemuseums.org>
2017-11-27 11:45:29 -0500

    Adding Linux start scripts to repo

---
+++ _HelpfulThings/electronstart.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sleep 15
+/home/studio/Documents/WeAreNature/FutureActionPoll/node_modules/electron-prebuilt/dist/electron /home/studio/Documents/WeAreNature/FutureActionPoll/
---
+++ _HelpfulThings/npmstart.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd /home/studio/Documents/WeAreNature/FutureActionPoll
+npm start
